d385994a4c2882b9f15efc73b174e9aca81e0079
Diego Alves <diegocaboclo@hotmail.com>
2024-07-19 09:34:36 -0300

    tratamento de erros

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -12,31 +12,34 @@ def randomNumber():
     return computerChoice
 
 def startGame():
-    try:   
-        getComputerChoice = randomNumber()
-        print('hello there, welcome to the rock paper and scissor game!\n')
-        userChoice = input('please select your choice between:  Rock, Paper and Scissor ') 
-    except Exception as e:
-        if userChoice != string and userChoice != 'rock' and userChoice != 'paper' and userChoice != 'scissor':
-            print('Just String are acceptable, try again')
+    print('hello there, welcome to the rock paper and scissor game!\n')
+    #Equanto userChoice estiver no arr, não tera problemas
+    while True:
+        try:   
+            userChoice = input('please select your choice between:  Rock, Paper and Scissor ')
+            userChoice = userChoice.lower()
+            if userChoice not in arr:
+                raise ValueError('Please select a valid choice between: Rock, Paper and Scissor')
+            break
+        except Exception as e: 
+            print(f'Unexpected Error {e}')
 
+    getComputerChoice = randomNumber()
     
     # draw conditions
     if userChoice == getComputerChoice:
         print(f'thats a draw, you have a {userChoice}')
 
-    elif userChoice == 'rock' and getComputerChoice == 'scissor':
-        print(f'Congratulations! you choice {userChoice} and win')
+    # win conditions
+    elif (userChoice == 'rock' and getComputerChoice == 'scissor') or \
+         (userChoice == 'paper' and getComputerChoice == 'rock') or \
+         (userChoice == 'scissor' and getComputerChoice == 'paper'):
+        print(f'Congratulations! You chose {userChoice} and win')
 
-    elif userChoice == 'paper' and getComputerChoice == 'rock':
-        print(f'Congratulations! you choice {userChoice} and win')
-    
-    elif userChoice == 'scissor' and getComputerChoice == 'paper':
-        print(f'Congratulations! you choice {userChoice} and win')
 
     else:
         print('sorry i guess you fail this time.')
 
-
+    
 
 startGame()
